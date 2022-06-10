bf786d21e7e5d9543195a98f4ba69992e85001e5
donderileo <lrodrigues@stepwise.com.br>
2022-06-10 09:12:02 -0300

    Adicionando assets e bibliotecas de UI

--- src/main.js
+++ src/main.js
@@ -6,10 +6,13 @@ import router from "./router";
 import PrimeVue from "primevue/config";
 import ProgressSpinner from "primevue/progressspinner";
 
+import "@/assets/scss/_theme.scss"; //theme
+
 const app = createApp(App);
 app.use(PrimeVue);
 app.use(createPinia());
 app.use(router);
 
 app.component("ProgressSpinner", ProgressSpinner);
+
 app.mount("#app");
